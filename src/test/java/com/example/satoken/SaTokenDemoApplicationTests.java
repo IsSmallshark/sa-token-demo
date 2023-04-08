6e3fe2f8a62eceb42f46ac8211b2b105dce71b43
xsq <x15756078813@163.com>
2023-04-08 11:32:08 +0800

    init

--- src/test/java/com/example/satoken/SaTokenDemoApplicationTests.java
+++ src/test/java/com/example/satoken/SaTokenDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SaTokenDemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
