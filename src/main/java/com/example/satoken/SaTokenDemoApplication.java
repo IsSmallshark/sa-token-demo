2edb094ab15f6a6327f2e35c085de86982003daf
xsq <x15756078813@163.com>
2023-05-11 15:04:24 +0800

    测试demo

--- src/main/java/com/example/satoken/SaTokenDemoApplication.java
+++ src/main/java/com/example/satoken/SaTokenDemoApplication.java
@@ -1,13 +1,19 @@
 package com.example.satoken;
 
+import cn.dev33.satoken.SaManager;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author xsq
+ */
 @SpringBootApplication
 public class SaTokenDemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JsonProcessingException {
         SpringApplication.run(SaTokenDemoApplication.class, args);
+        System.out.println("启动成功：Sa-Token配置如下：" + SaManager.getConfig());
     }
 
 }
